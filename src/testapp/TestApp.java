08e13ba560bde2b389e689c5b3e1848a7ecdce3c
DigitalDD <DigitalDrugDealer2@gmail.com>
2018-04-02 20:17:58 +0300

    test2 lab1

--- src/testapp/TestApp.java
+++ src/testapp/TestApp.java
@@ -3,8 +3,10 @@ package testapp;
 public class TestApp {
 
 public static void main(String[] args) {
-
 // TODO code application logic here
+float a=10;
+float b=(float) 12.5;
+System.out.println("a+b="+(a+b));
 System.out.println("GIT test application");
 }
 }
