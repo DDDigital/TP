e1456b86ac15496a072ea42331041168c95fb2cb
DigitalDD <DigitalDrugDealer2@gmail.com>
2018-05-01 18:37:14 +0300

    +-print2

---
+++ src/testapp/SimpleCalc.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testapp;
+
+/**
+ *
+ * @author Andrii
+ */
+public class SimpleCalc {
+    
+private double a;
+private double b;
+
+public SimpleCalc(){
+this.a=6;
+this.b=6;
+}
+public double getSum() {
+    
+return this.a+this.b;
+}
+public double getRizn() {
+return this.a-this.b;
+}
+
+}
