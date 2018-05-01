f1fae1f063fe06a4f698422a435dcd57a119ac22
DigitalDD <DigitalDrugDealer2@gmail.com>
2018-05-01 18:06:08 +0300

    Normal

--- src/testapp/TestApp.java
+++ src/testapp/TestApp.java
@@ -13,25 +13,4 @@ System.out.println("a+b="+(a+b));
 System.out.println("a-b="+(a-b));
 System.out.println("GIT test application");
 }
-
-public class SimpleCalc {
-
-private double a;
-private double b;
-
-SimpleCalc(double p1,double p2) {
-this.a=p1;
-this.b=p2;
-}
-
-public double getSum() {
-
-return this.a+this.b;
-}
-public double getRizn() {
-
-return this.a-this.b;
-}
-
-}
 }
