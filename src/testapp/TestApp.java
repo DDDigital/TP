4e2d0d9ba22648334f325f0f7b554194947dd6cf
DigitalDD <DigitalDrugDealer2@gmail.com>
2018-05-08 18:13:40 +0300

    New 3

--- src/testapp/TestApp.java
+++ src/testapp/TestApp.java
@@ -12,5 +12,7 @@ double b = Double.valueOf(in.nextLine());
 System.out.println("a+b="+(a+b));
 System.out.println("a-b="+(a-b));
 System.out.println("GIT test application");
+SimpleCalc A = new SimpleCalc());
+System.out.println(A.getSum());
 }
 }
